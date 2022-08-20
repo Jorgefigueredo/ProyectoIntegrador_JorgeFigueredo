0f7b3aa22e006e55b619bffd27e86074cf8b67fc
Jorge Figueredo <Figueredojorge@outlook.com>
2022-08-20 17:59:50 -0300

    banner

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { SocialComponent } from './components/social/social.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SocialComponent
   ],
   imports: [
     BrowserModule
