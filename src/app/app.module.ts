f45c4f66373b48b379df0070ff1fc7e17e0bd1b1
Jorge Figueredo <Figueredojorge@outlook.com>
2022-08-22 18:01:13 -0300

    ACERCADE

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { SocialComponent } from './components/social/social.component';
+import { LogoAPComponent } from './components/logo-ap/logo-ap.component';
+import { BannerComponent } from './components/banner/banner.component';
+import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    SocialComponent
+    SocialComponent,
+    LogoAPComponent,
+    BannerComponent,
+    AcercaDeComponent,
   ],
   imports: [
     BrowserModule
